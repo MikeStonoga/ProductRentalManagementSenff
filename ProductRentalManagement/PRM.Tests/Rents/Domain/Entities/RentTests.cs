c134ad240b03197fefca92ec3e3e3166bdebd06a
Mike Stonoga Machado <mikestonoga@hotmail.com>
2020-08-17 13:08:37 -0300

    PRM-0000

--- ProductRentalManagement/PRM.Tests/Rents/Domain/Entities/RentTests.cs
+++ ProductRentalManagement/PRM.Tests/Rents/Domain/Entities/RentTests.cs
@@ -13,7 +13,7 @@ namespace PRM.Tests.Rents.Domain.Entities
     public class RentTests
     {
         [Fact]
-        public void CreateRentSuccessfuly()
+        public void CreateRentSuccessfully()
         {
             // Arrange
             var rentPeriod = new DateRange(DateTime.Now, DateTime.Now.AddDays(10));
